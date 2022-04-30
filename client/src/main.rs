8be01ab0f4cc9f16241cfd1ae96944af0ccbc90c
jero-at-github <jlopeznavarro@gmail.com>
2022-04-30 20:21:34 +0200

    Implemented example for transfering a file

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, client!");
+use std::error::Error;
+
+use tokio::{
+    fs,
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    net::TcpStream,
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut stream = TcpStream::connect("localhost:8080").await?;
+    //stream.write_all(b"hello world!").await?;
+
+    let mut reader = BufReader::new(stream);
+    let mut reader_content = Vec::new();
+    reader.read_to_end(&mut reader_content).await.unwrap();
+
+    fs::write("samples/client/received.txt", reader_content).await?;
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,25 @@
-use std::net::SocketAddr;
+use std::{io::Read, net::SocketAddr};
 
-use tokio::{net::TcpListener, sync::broadcast};
+use tokio::{
+    fs,
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+    sync::broadcast,
+};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (mut socket, addr) = listener.accept().await.unwrap();
 
-    let (tx, _rx) = broadcast::channel::<(String, SocketAddr)>(1);
+    let (socket_reader, mut socket_writer) = socket.split();
+
+    let contents = fs::read("samples/server/data.txt").await?;
+
+    // let mut buffer = BytesMut::with_capacity(10);
+
+    socket_writer.write_all(contents.as_slice()).await.unwrap();
+    //let (tx, _rx) = broadcast::channel::<(String, SocketAddr)>(1);
+
+    Ok(())
 }
