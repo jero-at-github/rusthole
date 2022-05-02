6c6ca1e20cc1e449ecc8bca71d30ebeb464c419d
jero-at-github <jlopeznavarro@gmail.com>
2022-05-02 19:45:29 +0200

    Added secret phrase generator

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,5 +1,8 @@
 use clap::{Parser, Subcommand};
+use rand::Rng;
 use std::error::Error;
+use std::fs::File as StdFile;
+use std::io::BufReader as StdBufReader;
 use tokio::{
     fs::{self, File},
     io::{AsyncReadExt, AsyncWriteExt, BufReader},
@@ -18,6 +21,8 @@ struct Args {
 enum Commands {
     /// Send a file
     Send { path: String },
+    /// Send a file in test mode
+    TestSend,
     /// Receives a file
     Receive,
 }
@@ -27,12 +32,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     match &args.command {
-        Commands::Send { path } => exec_server(path.clone()).await,
-        Commands::Receive => exec_client().await,
+        Commands::Send { path } => exec_sender(path.clone()).await,
+        Commands::TestSend => exec_sender("./samples/client/received.mp4".into()).await,
+        Commands::Receive => exec_receiver().await,
     }
 }
 
-async fn exec_client() -> Result<(), Box<dyn Error>> {
+async fn exec_receiver() -> Result<(), Box<dyn Error>> {
     let host = "88.152.112.218";
     let port = 8080;
     let path = "../../samples/client/received.mp4";
@@ -53,13 +59,16 @@ async fn exec_client() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn exec_server(path: String) -> Result<(), Box<dyn std::error::Error + 'static>> {
-    // let ip = public_ip::addr().await.unwrap();
+async fn exec_sender(path: String) -> Result<(), Box<dyn Error>> {
     let ip = "127.0.0.1";
     let port = 8080;
 
     // Start listener
     let listener = TcpListener::bind(format!("{}:{}", ip, port)).await.unwrap();
+
+    // Generate and print the secret phrase
+    let secret_phrase = get_phrase().unwrap();
+    print_secret_phrase(secret_phrase);
 
     // Wait for connection
     println!("Accepting connection at {}:{}", ip, port);
@@ -71,4 +80,33 @@ async fn exec_server(path: String) -> Result<(), Box<dyn std::error::Error + 'st
     socket_writer.write_all(contents.as_slice()).await.unwrap();
 
     Ok(())
+}
+
+fn print_secret_phrase(secret_phrase: String) {
+    println!("Rusthole code is: {}", secret_phrase);
+    println!("On the other computer, please run:");
+    println!("");
+    println!("rusthole receive {}", secret_phrase);
+}
+
+fn get_phrase() -> Result<String, Box<dyn Error>> {
+    let mut rng = rand::thread_rng();
+
+    let json_file = StdFile::open("./bip39-es.json")?;
+    let json_reader = StdBufReader::new(json_file);
+    let bip39_list: serde_json::Value =
+        serde_json::from_reader(json_reader).expect("JSON was not well-formatted");
+
+    let phrase = format!(
+        "{}-{}-{}",
+        rng.gen_range(0..=9 as u8),
+        bip39_list["list"][rng.gen_range(0..=2047)]
+            .as_str()
+            .unwrap(),
+        bip39_list["list"][rng.gen_range(0..=2047)]
+            .as_str()
+            .unwrap()
+    );
+
+    Ok(phrase)
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,5 +2,15 @@ use tokio::{fs, io::AsyncWriteExt, net::TcpListener};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let ip = "127.0.0.1";
+    let port = 8080;
+
+    // Start listener
+    let listener = TcpListener::bind(format!("{}:{}", ip, port)).await.unwrap();
+
+    // Wait for connection
+    println!("Sync server accepting connection at {}:{}", ip, port);
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+
     Ok(())
 }
